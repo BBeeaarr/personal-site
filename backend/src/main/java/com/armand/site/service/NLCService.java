fb57a4339271b279fb8971bf799033f604f20284
Ourmond <armandrv@uci.edu>
2026-04-29 23:55:57 -0700

    predict service method

--- backend/src/main/java/com/armand/site/service/NLCService.java
+++ backend/src/main/java/com/armand/site/service/NLCService.java
@@ -1,13 +1,16 @@
 package com.armand.site.service;
 
-import com.armand.site.api.dto.NLC.NLCCurrentCase;
-import com.armand.site.api.dto.NLC.NLCRequest;
-import com.armand.site.api.dto.NLC.NLCResponse;
+import com.armand.site.api.dto.NLC.*;
 import com.armand.site.repository.NLCRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class NLCService {
@@ -19,7 +22,43 @@ public class NLCService {
     {
         String challengeId = request.challengeId();
         int schemaVersion = request.schemaVersion();
-        OffsetDateTime generatedAt = request.generatedAt();
+        OffsetDateTime generatedAt = request.generatedAt(); //pretty sure this is gonna be a string
         List<NLCCurrentCase> nlcCaseList = request.cases();
+        List<NLCPrediction> responses = new ArrayList<NLCPrediction>();
+
+
+        for (NLCCurrentCase nlcCurrentCase : nlcCaseList)
+        {
+            String caseId = nlcCurrentCase.caseId();
+            NLCStudy currentStudy = nlcCurrentCase.currentStudy();
+            LocalDate currentDate = currentStudy.studyDate(); //pretty sure this is gonna be a string
+            String currentDescription = currentStudy.studyDescription();
+            RestTemplate restTemplate = new RestTemplate();
+            String url = PREDICTION_SERVICE_URL;
+
+
+            for (NLCStudy priorStudy : nlcCurrentCase.priorStudies())
+            {
+                String priorDescription = priorStudy.studyDescription();
+                String priorId = priorStudy.studyId();
+                LocalDate priorDate = priorStudy.studyDate();
+                Map<String, Object> body = Map.of(
+                        "query", currentDescription,
+                        "text", priorDescription,
+                        "query_date", currentDate,
+                        "text_date", priorDate
+                );
+
+
+                ResponseEntity<Integer> response = restTemplate.postForEntity(url, body, Integer.class);
+
+                responses.add(new NLCPrediction(
+                        caseId,
+                        priorId,
+                        response.getBody() == 1 ? true : false
+                ));
+            }
+        }
+        return new NLCResponse(responses);
     }
 }
